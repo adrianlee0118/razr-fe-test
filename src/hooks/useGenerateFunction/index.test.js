6afae53ec3eebb296f058bc694a3a6cf49d34466
Adrian Lee <adrianlee0118@gmail.com>
2020-08-19 01:41:32 -0700

    added a variety of tests for API fetching functions

--- src/hooks/useGenerateFunction/index.test.js
+++ src/hooks/useGenerateFunction/index.test.js
@@ -1,22 +1,18 @@
 import { fetchDimensions } from "./index";
 import { Random_100_URL } from "../../constants";
-import axios from "axios";
 
-jest.mock("axios");
+it("fetches 100 random numbers from API", async () => {
+  const numbers = await fetchDimensions(Random_100_URL);
+  expect(numbers.length - 1).toEqual(100);
+});
 
-describe("fetchDimensions", () => {
-  it("fetches data successfully from an API", async () => {
-    const result = { data: "1\r\n23\r\n3\r\n56" };
-    axios.get.mockImplementationOnce(() => Promise.resolve(result));
-    await expect(fetchDimensions(Random_100_URL)).resolves.toEqual(result);
-    expect(axios.get).toHaveBeenCalledWith(Random_100_URL);
-  });
-
-  it("fetches erroneously data from an API", async () => {
-    const errorMessage = "Network Error";
-    axios.get.mockImplementationOnce(() =>
-      Promise.reject(new Error(errorMessage))
-    );
-    await expect(fetchDimensions(Random_100_URL)).rejects.toThrow(errorMessage);
-  });
+it("fetches numbers that fall only between 1 and 100", async () => {
+  const numbers = await fetchDimensions(Random_100_URL);
+  let mini = 50,
+    maxi = 50;
+  numbers.forEach((num) =>
+    num > maxi ? (maxi = num) : num < mini ? (mini = num) : num
+  );
+  expect(mini).toBeGreaterThanOrEqual(1);
+  expect(maxi).toBeLessThanOrEqual(100);
 });
