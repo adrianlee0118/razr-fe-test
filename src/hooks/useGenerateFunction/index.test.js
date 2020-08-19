2accb9d3a877db92c1321df499b6725e972f2c2c
Adrian Lee <adrianlee0118@gmail.com>
2020-08-19 10:25:32 -0700

    wrapped fetchDimension test components

--- src/hooks/useGenerateFunction/index.test.js
+++ src/hooks/useGenerateFunction/index.test.js
@@ -1,13 +1,15 @@
 import { fetchDimensions } from "./index";
 import { Random_100_URL } from "../../constants";
 
-it("fetches 100 random numbers from API", async () => {
-  const numbers = await fetchDimensions(Random_100_URL);
-  expect(numbers.length).toEqual(100);
-});
+describe("fetchDimensions", () => {
+  it("fetches 100 random numbers from API", async () => {
+    const numbers = await fetchDimensions(Random_100_URL);
+    expect(numbers.length).toEqual(100);
+  });
 
-it("fetches numbers that fall only between 1 and 100", async () => {
-  const numbers = await fetchDimensions(Random_100_URL);
-  expect(Math.min(...numbers)).toBeGreaterThanOrEqual(1);
-  expect(Math.max(...numbers)).toBeLessThanOrEqual(100);
+  it("fetches numbers that fall only between 1 and 100", async () => {
+    const numbers = await fetchDimensions(Random_100_URL);
+    expect(Math.min(...numbers)).toBeGreaterThanOrEqual(1);
+    expect(Math.max(...numbers)).toBeLessThanOrEqual(100);
+  });
 });
