104963ca64a1e610acc125ca02031f118bcb8f6c
Adrian Lee <adrianlee0118@gmail.com>
2020-08-19 13:32:02 -0700

    added API mock testing

--- src/hooks/useGenerateFunction/index.test.js
+++ src/hooks/useGenerateFunction/index.test.js
@@ -1,7 +1,11 @@
 import { fetchDimensions } from "./index";
 import { Random_100_URL } from "../../constants";
+import axios from "axios";
+
+jest.mock("axios");
 
 describe("fetchDimensions", () => {
+  /*
   it("fetches 100 random numbers from API", async () => {
     const numbers = await fetchDimensions(Random_100_URL);
     expect(numbers.length).toEqual(100);
@@ -11,5 +15,13 @@ describe("fetchDimensions", () => {
     const numbers = await fetchDimensions(Random_100_URL);
     expect(Math.min(...numbers)).toBeGreaterThanOrEqual(1);
     expect(Math.max(...numbers)).toBeLessThanOrEqual(100);
+  });
+  */
+  it("returns a list of numbers", async () => {
+    axios.get.mockResolvedValue({
+      data: "1\n32\n12\n98\n2",
+    });
+    const numbers = await fetchDimensions(Random_100_URL);
+    expect(numbers).toEqual([1, 32, 12, 98, 2]);
   });
 });
